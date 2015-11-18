eae28cc48b45d674904200cd5449f29088ee880e
Preetham N <preetham.in.1997@gmail.com>
2015-11-18 00:46:29 -0800

    Setup button

---
+++ app/src/main/java/com/rhombus/songguesser/GuesserActivity.java
@@ -0,0 +1,13 @@
+package com.rhombus.songguesser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuesserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guesser);
+    }
+}
--- app/src/main/java/com/rhombus/songguesser/MainActivity.java
+++ app/src/main/java/com/rhombus/songguesser/MainActivity.java
@@ -1,7 +1,10 @@
 package com.rhombus.songguesser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.playbutton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GuesserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button initButton = (Button) findViewById(R.id.initbutton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ///TODO: SQLite stuff!!!
+            }
+        });
     }
 }
